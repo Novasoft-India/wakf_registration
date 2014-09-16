759ff271b7bdf8c0c31450a7935dafaf9091f7d8
Hashir Haris <hashir.haris@gmail.com>
2014-09-16 08:11:08 +0530

    Modifications merged to ver 2.0

--- wakf_immovable_property.py
+++ wakf_immovable_property.py
@@ -1,5 +1,5 @@
-from osv import osv
-from osv import fields
+from openerp.osv import osv
+from openerp.osv import fields
 
 class wakf_immovableproperty(osv.osv):
     """
@@ -7,6 +7,35 @@ class wakf_immovableproperty(osv.osv):
     """
     _name = 'wakf.immovableproperty'
     _description = 'wakf.immovableproperty'
+    
+    def get_convert(self, cr, uid, ids, fields, arg, context):
+        converted = {}
+        for record in self.browse(cr, uid, ids):
+            area_non_standard = record.area
+            unit = record.units_id.code
+            ################ LookUp Table ################
+            # 1 Acre = 100.01467985501355 Cent
+            # 1 Sq.Feet = 0.002296 Cent
+            # 1 Sq.Metres = 0.024688 Cent
+            # 1 Sq.Yard = 0.020664 Cent
+            # 1 Hectares = 247.1363878343195 Cent
+            # 1 Kottah = 1.65311998677504 Cent
+            # 1 Kanal = 247.93218242767915 Cent
+            # 1 Marla = 12.396608987519919 Cent
+            # 1 Ares = 2.4704960000259897 Cent
+            ##############################################
+            if unit == 'ACRE': value = 100.01467
+            if unit == 'SQFT': value = 0.002296
+            if unit == 'SQMT': value = 0.024688
+            if unit == 'SQYD': value = 0.020664
+            if unit == 'HCTR': value = 247.13638
+            if unit == 'KOTA': value = 1.653119
+            if unit == 'KANL': value = 247.932182
+            if unit == 'MRLA': value = 12.396608
+            if unit == 'ARES': value = 2.470496
+            area_converted = area_non_standard * value
+            converted[record.id]= area_converted
+        return converted
  
     _columns = {
             'wakf_id':fields.many2one('res.partner','Wakf Name',ondelete='set null'), 
@@ -35,8 +64,8 @@ class wakf_immovableproperty(osv.osv):
             'patta_no':fields.char('Patta No',size=8,required=False),
             'district_id':fields.many2one('wakf.district','District',ondelete='set null'),
             'taluk_id':fields.many2one('wakf.taluk','Taluk',ondelete='set null'),
-            'village_id':fields.many2one('wakf.village','Village',ondelete='set null'),           
-            
+            'village_id':fields.many2one('wakf.village','Village',ondelete='set null'), 
+            'converted_area':fields.function(get_convert,string='Area in Cent',store=True,type='float',method=False),
         }
 wakf_immovableproperty()
 
